70b03f968a6a1313ebe5719b4c5e237442f0fd48
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2023-04-17 21:33:27 +0700

    Initial commit

---
+++ FileExplorer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace FileExplorer.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string mainDiskName;
+
+        public string MainDiskName 
+        { 
+            get => mainDiskName;
+            set
+            {
+                mainDiskName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MainDiskName)));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            MainDiskName = Environment.SystemDirectory;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
