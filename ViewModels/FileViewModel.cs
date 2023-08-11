814b8c5065c1cad277509cd5f0e1562195cbd4c7
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2023-08-11 15:40:50 +0700

    Added file types

--- ViewModels/FileViewModel.cs
+++ ViewModels/FileViewModel.cs
@@ -18,7 +18,7 @@ namespace FileExplorer.ViewModels
             DateOfCreation = fileInfo.CreationTime.ToShortDateString() + " " + fileInfo.CreationTime.ToShortTimeString();
             if (fileInfo.GetType().ToString() == "System.IO.FileInfo")
             {
-                Type = "Файл";
+                Type = GetFileType(fileInfo);
                 IsSystemFolder = false;
             }
             Size = GetFileSize(fileInfo);            
@@ -33,11 +33,38 @@ namespace FileExplorer.ViewModels
 
             while (size > 999 && i < 3)
             {
-                size = size / 1024;
+                size /= 1024;
                 i++;
             }
 
             return (size).ToString() + units[i];
+        }
+        
+        private static string GetFileType(FileInfo fileInfo)
+        {
+            string extention = Path.GetExtension(fileInfo.FullName);
+            string result = extention switch
+            {
+                ".txt" or ".log" or ".gitattributes" or ".gitignore" => "Текстовый документ",
+                ".rtf" => "Формат RTF",
+                ".ico" => "Значок",
+                ".docx" or ".doc" => "Документ Microsoft Word",
+                ".pptx" => "Презентация Microsoft PowerPoint",
+                ".csv" => "Файл Microsoft Excel",
+                ".json" or ".bin" => extention.Substring(1).ToUpperInvariant() + " file",
+                ".ini" => "Параметры конфигурации",
+                ".vsix" => "Microsoft Visual Studio Extention",
+                ".sln" => "Microsoft Visual Studio Solution",
+                ".cpp" => "C++ source file",
+                ".cs" => "C# Source File",
+                ".csproj" => "C# Project File",
+                ".torrent" => "Торрент файл",
+                ".7z" => "7z Archive",
+                ".exe" => "Приложение",
+                ".dll" => "Расширение приложения",
+                _ => "Файл " + extention.Substring(1).ToUpperInvariant(),
+            };
+            return result;
         }
     }
 }
